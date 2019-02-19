1d08ef2507c9bc6cf9ba7c942525eeb83c741685
James Musyoka <jamesmu475@gmail.com>
2019-02-19 14:45:42 +0300

    putted my new database_url

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
  SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:37472377@localhost/flaskblog'
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
   pass
 
 
