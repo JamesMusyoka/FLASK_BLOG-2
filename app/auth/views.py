0d56beaf9b35d60a297afb41274a321d733cc06d
James Musyoka <jamesmu475@gmail.com>
2019-02-19 14:48:51 +0300

    returned my  development

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
         flash('Invalid username or Password')
 
     title = "Blog login"
-    return render_template('auth/login.html',login_form = login_form,title=title )
+    return render_template('templates/quotes.html',login_form = login_form,title=title )
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
